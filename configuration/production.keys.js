a8ade9f97a892e5abe48ad1a18f3ec4aa7502778
Wolodymyr <ddux05@gmail.com>
2021-02-17 21:15:49 +0200

    devided into two files (development keyses/production keyses (for secure)

---
+++ configuration/production.keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    MONGO_URI: process.env.MONGO_URI,
+    SESSION_SECRET_VALUE: process.env.SESSION_SECRET_VALUE,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    BASE_URL: process.env.BASE_URL,
+    MIN_PASS_SYMBOLS: 5,
+    MAX_PASS_SYMBOLS: 21
+}
